06115fb2bacd92f44172072bebf0379025b0d383
Rudeka <thotubox@gmail.com>
2017-04-04 20:56:14 +0300

    Add greeting dialog

---
+++ ChatBot/Dialogs/GreetingDialog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace ChatBot.Dialogs
+{
+    [Serializable]
+    public class GreetingDialog : IDialog
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.UserData.Clear();
+            await context.PostAsync("Hi I'm mebel and decor Chat Bot");
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+            string userName;
+            var getName = false;
+            context.UserData.TryGetValue<string>("Name", out userName);
+            context.UserData.TryGetValue<bool>("GetName", out getName);
+
+            if (getName)
+            {
+                userName = message.Text;
+                context.UserData.SetValue<string>("Name", userName);
+                context.UserData.SetValue<bool>("GetName", false);
+            }
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                await context.PostAsync("What is your name?");
+                context.UserData.SetValue<bool>("GetName", true);
+            }
+            else
+            {
+                await context.PostAsync($"Hi {userName}. How can I help you today?");
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
